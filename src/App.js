614c08f13a8f66a13cf5316482c46bb0dfa552ca
Soban Saeed <soban.saeed@devsinc.com>
2024-07-22 23:49:05 +0500

    Completed Functionality with Images

--- src/App.js
+++ src/App.js
@@ -1,40 +1,70 @@
 import './App.css';
 import { useState } from 'react';
+import breadbun from './burgerimages/breadbun.svg'
+import salad from './burgerimages/salad.svg'
+import bacon from './burgerimages/bacon.svg'
+import cheese from './burgerimages/cheese.svg'
+import meat from './burgerimages/meat.svg'
 
 function App() {
-  // Use States
+  // VegData
+  const [vegData, setVegData] = useState([
+    { name: 'Salad', cost: 2, count: 0 , img: salad},
+    { name: 'Bacon', cost: 3, count: 0 , img: bacon},
+    { name: 'Cheese', cost: 4, count: 0 , img: cheese},
+    { name: 'Meat', cost: 5, count: 0 , img : meat}
+  ]);
+
+  // Use state for cost
   const [cost, setCost] = useState(4);
 
-  // Increment Cost Function
-  const incrementCost = (value) => {
-    setCost((cost) => cost + value);
-  }
+  // Increment Cost and Count Function
+  const incrementCost = (index) => {
+    const newVegData = [...vegData];
+    newVegData[index].count += 1;
+    setVegData(newVegData);
+    setCost(cost + newVegData[index].cost);
+    console.log(newVegData)
+  };
 
-  // Decrement Cost Function
-  const decrementCost = (value) => {
-    setCost((cost) => cost - value);
-  }
-
-  // Veg Array
-  const VegData = [
-    { name: 'Salad', value: 2 },
-    { name: 'Bacon', value: 3 },
-    { name: 'Cheese', value: 4 },
-    { name: 'Meat', value: 5 }
-  ]
+  // Decrement Cost and Count Function
+  const decrementCost = (index) => {
+    const newVegData = [...vegData];
+    if (newVegData[index].count > 0) {
+      newVegData[index].count -= 1;
+      setVegData(newVegData);
+      setCost(cost - newVegData[index].cost);
+      console.log(newVegData)
+    }
+  };
 
   // Rendering buttons
   const renderButtons = () => {
-    return VegData.map((item, index) => (
-      <div key={index}>
-        {item.name}: ${item.value}
+    return vegData.map((item, index) => (
+      <>
+      {item.name}: ${item.cost} (Count: {item.count})
         <br /><br />
-        <button onClick={() => incrementCost(item.value)}>Add</button>
-        <button onClick={() => decrementCost(item.value)}>Remove</button>
+        <button onClick={() => incrementCost(index)}>Add</button> &nbsp;
+        <button onClick={() => decrementCost(index)}>Remove</button>
         <br /><br />
-      </div>
+      </>
+
     ));
   };
+
+  // Rendering Photo
+  const renderPhoto = () => {
+    return vegData.map((item, index) =>
+      Array(item.count).fill(null).map((_, i) => (
+        <div key={`${index}-${i}`}>
+          <img src={item.img} alt={item.name} />
+          <br />
+        </div>
+      ))
+    );
+  };
+
+
 
   return (
     <div className='BurgerBuilder'>
@@ -42,6 +72,9 @@ function App() {
       <p className='Cost'><strong>Current Cost: </strong> ${cost} </p>
       {renderButtons()}
 
+      <img src={breadbun} alt='topbun' height={200} width={200}></img> <br></br>
+      {renderPhoto()}
+      <img src={breadbun} alt='bottombun' className='bottom-bun' height={200} width={200}></img>
     </div>
   );
 }
