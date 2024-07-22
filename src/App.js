1e50bcc4fb6b1af9e2f06346f54a2d7c83cf17e6
Soban Saeed <soban.saeed@devsinc.com>
2024-07-22 21:07:28 +0500

    Button Functionality with UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  // Use States
+  const [cost, setCost] = useState(4);
+
+  // Increment Cost Function
+  const incrementCost = (value) => {
+    setCost((cost) => cost + value);
+  }
+
+  // Decrement Cost Function
+  const decrementCost = (value) => {
+    setCost((cost) => cost - value);
+  }
+
+  // Veg Array
+  const VegData = [
+    { name: 'Salad', value: 2 },
+    { name: 'Bacon', value: 3 },
+    { name: 'Cheese', value: 4 },
+    { name: 'Meat', value: 5 }
+  ]
+
+  // Rendering buttons
+  const renderButtons = () => {
+    return VegData.map((item, index) => (
+      <div key={index}>
+        {item.name}: ${item.value}
+        <br /><br />
+        <button onClick={() => incrementCost(item.value)}>Add</button>
+        <button onClick={() => decrementCost(item.value)}>Remove</button>
+        <br /><br />
+      </div>
+    ));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='BurgerBuilder'>
+      <h1>Welcome to Burger Builder</h1>
+      <p className='Cost'><strong>Current Cost: </strong> ${cost} </p>
+      {renderButtons()}
+
     </div>
   );
 }
